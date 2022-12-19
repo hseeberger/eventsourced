2442ac2f276e5afdc2d45b695165cbcb4fec8636
Heiko Seeberger <git@heikoseeberger.de>
2022-12-19 14:01:26 +0100

    build: optimize optional deps, fix GH test workflow

--- build.rs
+++ build.rs
@@ -1,18 +1,32 @@
-use anyhow::{Context, Result};
+#[cfg(feature = "nats")]
+use anyhow::Context;
+use anyhow::Result;
+#[cfg(feature = "nats")]
 use std::{
     ffi::OsStr,
     path::{Path, PathBuf},
     vec,
 };
+#[cfg(feature = "nats")]
 use walkdir::WalkDir;
 
+#[cfg(feature = "nats")]
 const PROTOS: &str = "proto";
 
 fn main() -> Result<()> {
+    #[cfg(feature = "nats")]
+    compile_protos()?;
+
+    Ok(())
+}
+
+#[cfg(feature = "nats")]
+fn compile_protos() -> Result<()> {
     let protos = list_protos(Path::new(PROTOS))?;
     prost_build::compile_protos(&protos, &[PROTOS]).context("Cannot compile protos")
 }
 
+#[cfg(feature = "nats")]
 fn list_protos(dir: &Path) -> Result<Vec<PathBuf>> {
     WalkDir::new(dir)
         .into_iter()
