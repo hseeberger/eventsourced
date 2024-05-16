855d0ae4ddbc4964a22d9ff1275407e828f0b09b
Heiko Seeberger <git@heikoseeberger.de>
2024-05-16 18:29:16 +0200

    refactor: use sqlx instead of tokio-postgres

--- eventsourced-postgres/src/lib.rs
+++ eventsourced-postgres/src/lib.rs
@@ -2,33 +2,22 @@
 //! [SnapshotStore](eventsourced::snapshot_store::SnapshotStore) implementations based upon [PostgreSQL](https://www.postgresql.org/).
 
 mod event_log;
+mod pool;
 mod snapshot_store;
 
 pub use event_log::{Config as PostgresEventLogConfig, PostgresEventLog};
 pub use snapshot_store::{Config as PostgresSnapshotStoreConfig, PostgresSnapshotStore};
 
-use bb8_postgres::{
-    bb8::{Pool, PooledConnection},
-    PostgresConnectionManager,
-};
 use error_ext::BoxError;
 use std::num::NonZeroU64;
 use thiserror::Error;
 
-type CnnPool<T> = Pool<PostgresConnectionManager<T>>;
-
-type Cnn<'a, T> = PooledConnection<'a, PostgresConnectionManager<T>>;
-
 /// Errors from the [PostgresEventLog] or [PostgresSnapshotStore].
 #[derive(Debug, Error)]
 pub enum Error {
-    /// Postgres error.
-    #[error("Postgres error: {0}")]
-    Postgres(String, #[source] tokio_postgres::Error),
-
-    /// Cannot get connection from pool.
-    #[error("cannot get connection from pool")]
-    GetConnection(#[source] bb8_postgres::bb8::RunError<tokio_postgres::Error>),
+    /// sqlx error.
+    #[error("{0}")]
+    Sqlx(String, #[source] sqlx::Error),
 
     /// Cannot convert an event to bytes.
     #[error("cannot convert an event to bytes")]
