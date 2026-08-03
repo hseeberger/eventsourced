05f853c78dc94b0a1135b6de462c4718fe9ec1b9
Heiko Seeberger <git@heikoseeberger.de>
2026-08-03 20:36:04 +0200

    fix(test): make test helper modules available to unit tests

--- eventsourced/src/snapshot_store.rs
+++ eventsourced/src/snapshot_store.rs
@@ -1,7 +1,8 @@
 //! Persistence for snapshots.
 
 pub mod noop;
-#[cfg(feature = "test")]
+#[cfg_attr(docsrs, doc(cfg(feature = "test")))]
+#[cfg(any(test, feature = "test"))]
 pub mod test;
 
 use bytes::Bytes;
